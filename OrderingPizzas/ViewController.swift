2808c784bb64e5eef52005745c433d6273c0f397
Zachary White <zacharywhite@maed-74.local>
2017-03-09 12:52:47 -0700

    Added Pizza Class

--- OrderingPizzas/ViewController.swift
+++ OrderingPizzas/ViewController.swift
@@ -18,19 +18,19 @@ protocol PizzaInterface  {
 
 
 class ViewController: UIViewController {
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+
+class Pizza {
+    func calculatePizzaTime(pizzas: [Pizza]) -> Int {
+        
+    }
+    
+    func addPizza(pizza: Pizza) -> Void {
+        
+    }
+}
